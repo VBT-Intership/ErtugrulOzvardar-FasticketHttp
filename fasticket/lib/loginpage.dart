eccb7e63626b5b1a70d4c60830f906e8a46d175b
ertugrulozvardar <ertugrul.ozvardar@ozu.edu.tr>
2020-09-01 23:35:17 +0300

    LoginPage finished. Profile Page added. And transition between Login page and Profile page is handled.

--- fasticket/lib/loginpage.dart
+++ fasticket/lib/loginpage.dart
@@ -1,4 +1,6 @@
+import 'package:fasticket/profile.dart';
 import 'package:flutter/material.dart';
+
 
 
 class LoginPage extends StatefulWidget {
@@ -10,23 +12,33 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: buildColumn()
+    body: buildColumn(),
 
     );
   }
 
   Column buildColumn() {
-    return Column(children: [
-    Expanded(flex:4,child: buildTitleTextAndIcon()),
-    Expanded(flex:2,child: buildEmail()),
-    Expanded(flex:2,child: buildPassword()),
-    Expanded(flex:2,child: buildLoginButton())],);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+    Expanded(flex:1,child: buildLoginPageImage()),
+    Expanded(flex:2,child: paddingTitleIcon()),
+    Expanded(flex:1,child: buildEmail()),
+    Expanded(flex:1,child: buildPassword()),
+    Expanded(flex:1,child: paddingLoginButton()),
+    Expanded(flex:1,child: buildLoginPageImage()),],);
   }
 
-  RaisedButton buildLoginButton() => RaisedButton(onPressed: () {}, child: const Text("Login"),color: Colors.blue,);
+  SizedBox buildLoginPageImage() => SizedBox(child: Image(image: AssetImage('assets/loginpage2.png'),fit:BoxFit.fitWidth));
 
+  Padding paddingTitleIcon() {
+    return Padding(
+    padding: const EdgeInsets.symmetric(horizontal:75),
+    child: buildTitleTextAndIcon(),
+  );
+  }
 
-  Row buildTitleTextAndIcon() {
+    Row buildTitleTextAndIcon() {
     return Row(
     children: [
       buildTitle(),
@@ -35,12 +47,10 @@ class _LoginPageState extends State<LoginPage> {
   );
   }
 
-  Text buildTitle() => Text("FASTICKET", style: TextStyle(color: Colors.blue[600], fontWeight: FontWeight.bold, fontSize: 35),);
-
+  Text buildTitle() => Text("FASTICKET", style: TextStyle(color: Colors.blue[600], fontWeight: FontWeight.bold, fontSize: 40),);
   Icon buildIcon() => Icon(Icons.blur_on, color:Colors.blue[400], size: 50,);
 
-
-  TextField buildEmail() {
+ TextField buildEmail() {
     return TextField(decoration:
   InputDecoration(icon: Icon(Icons.mail_outline, color: Colors.grey[600]),
                   hintText: "Enter your email:"),);
@@ -52,5 +62,18 @@ class _LoginPageState extends State<LoginPage> {
                   hintText: "Password:"),obscureText: true,);
   }
 
+
+  Padding paddingLoginButton() {
+    return Padding(
+    padding: const EdgeInsets.only(right:90,left:90, bottom:55),
+    child: buildLoginButton(),
+  );
+  }
+  RaisedButton buildLoginButton() => RaisedButton(
+    onPressed: () {
+              Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProfilePage()));},
+    child: const Text("Login", style: TextStyle(color:Colors.white, fontWeight: FontWeight.bold, fontSize: 20),),color: Colors.blue, splashColor: Colors.deepOrangeAccent,);
 
 }
