186667f81f2ebdd8b5337da74f1cc9b0e56096e5
ertugrulozvardar <ertugrul.ozvardar@ozu.edu.tr>
2020-09-01 21:38:16 +0300

    Login page attributes reaaranged and initialized components grouped into extracted methods.

--- fasticket/lib/loginpage.dart
+++ fasticket/lib/loginpage.dart
@@ -9,15 +9,44 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Column(children: [
-      Text("BUSTICKET"),
-      TextField(decoration:
-      InputDecoration(icon: Icon(Icons.mail_outline, color: Colors.grey[600]),
-                      hintText: "Enter your email:"),),
-      TextField(decoration:
-      InputDecoration(icon: Icon(Icons.lock_outline, color: Colors.grey[600],),
-                      hintText: "Password:"),)],)
+    body: buildColumn()
 
     );
   }
+
+  Column buildColumn() {
+    return Column(children: [
+    buildTitleTextAndIcon(),
+    buildEmail(),
+    buildPassword()],);
+  }
+
+
+  Row buildTitleTextAndIcon() {
+    return Row(
+    children: [
+      buildTitle(),
+      buildIcon(),
+    ],
+  );
+  }
+
+  Text buildTitle() => Text("FASTICKET", style: TextStyle(color: Colors.blue[600], fontWeight: FontWeight.bold, fontSize: 35),);
+
+  Icon buildIcon() => Icon(Icons.blur_on, color:Colors.blue[400], size: 50,);
+
+
+  TextField buildEmail() {
+    return TextField(decoration:
+  InputDecoration(icon: Icon(Icons.mail_outline, color: Colors.grey[600]),
+                  hintText: "Enter your email:"),);
+  }
+
+  TextField buildPassword() {
+    return TextField(decoration:
+  InputDecoration(icon: Icon(Icons.lock_outline, color: Colors.grey[600],),
+                  hintText: "Password:"),);
+  }
+
+
 }
