27c6a4cbb4d0301f9380ff543899814c00823572
ertugrulozvardar <ertugrul.ozvardar@ozu.edu.tr>
2020-09-01 21:09:52 +0300

    Default components created.

--- fasticket/lib/loginpage.dart
+++ fasticket/lib/loginpage.dart
@@ -8,8 +8,16 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-    child: Text("Fasticket"),
+    return Scaffold(
+    body: Column(children: [
+      Text("BUSTICKET"),
+      TextField(decoration:
+      InputDecoration(icon: Icon(Icons.mail_outline, color: Colors.grey[600]),
+                      hintText: "Enter your email:"),),
+      TextField(decoration:
+      InputDecoration(icon: Icon(Icons.lock_outline, color: Colors.grey[600],),
+                      hintText: "Password:"),)],)
+
     );
   }
 }
