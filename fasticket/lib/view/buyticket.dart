b47947f010affa77cc7d0c35a93612541dbaaff6
ertugrulozvardar <ertugrul.ozvardar@ozu.edu.tr>
2020-09-03 00:11:00 +0300

    Extracted methods.

--- fasticket/lib/view/buyticket.dart
+++ fasticket/lib/view/buyticket.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fasticket/view/profile.dart';
+import 'package:fasticket/model/flightsmodel.dart';
 
 
 class BuyTicket extends StatefulWidget {
@@ -14,6 +14,7 @@ class _BuyTicketState extends State<BuyTicket> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text("Buy Ticket"), centerTitle: true,
         automaticallyImplyLeading: false,
         actions: [IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () {Navigator.pop(context);})],),
       body: Text("Tickets:")
